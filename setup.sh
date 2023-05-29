5e6d1c61d5f62668cf85500ee9c1d05f3c7bd91c
Niklas20114552 <83753407+Niklas20114552@users.noreply.github.com>
2023-05-29 19:46:46 +0200

    Update Text

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 echo -e "Das ist BETA Software.\nJa.\nMehr kommt jetzt auch nicht...\nIch wollts halt nur mal erwähnt haben.\n\nNun kommt aber das Wichtige:\n- Python muss installiert sein.\n- Pip sollte installiert sein.\n- Nur Linux wird unterstützt.\n- Sudo rechte werden für die Installation benötigt.\n"
 if [[ $1 == "--apt" ]]; then
     echo "Es wird APT-GET zum Installieren verwendet."
-    read -p "Press any key to continue" -rsn1
+    read -p "Drücke eine beliebige Taste zum fortfahren..." -rsn1
     echo "Und los gehts.."
     sudo apt-get install python-pyqt5 python3-pyqt5.qtwebengine python3-requests python3-pyqt5-sip
 elif [[ $1 == "--pacman" ]]; then
     echo "Es wird PACMAN zum Installieren verwendet."
-    read -p "Press any key to continue" -rsn1
+    read -p "Drücke eine beliebige Taste zum fortfahren..." -rsn1
     echo "Und los gehts.."
     sudo pacman -S --needed python-pyqt5 python-pyqt5-sip python-pyqt5-webengine python-requests
 elif [[ $1 == "--pip" ]]; then
     echo "Es wird PIP3 zum Installieren verwendet."
-    read -p "Press any key to continue" -rsn1
+    read -p "Drücke eine beliebige Taste zum fortfahren..." -rsn1
     echo "Und los gehts.."
     sudo pip3 install pyqt5 requests pyqtwebengine pyqt5-sip
 else
